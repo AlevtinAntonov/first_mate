f5d3bc3f57f2004b85eafa47f03e5c9258cc337d
AlevtinAntonov <alevtin_antonov@mail.ru>
2024-01-19 08:49:27 +0300

    ver_3.24.01.01

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import tkinter as tk
 
-# from app_model.db.db_config import db_config
 from app_view_model.login_page import LoginPage
-from app_view_model.start_page import StartPage
 
 if __name__ == '__main__':
-    # при установке запускаем db_config ля записи пути к базе данных из base.ini
-    # db_config()
     root = tk.Tk()
     root.withdraw()
-    # StartPage(root)
     LoginPage(root)
     root.deiconify()
     root.mainloop()
